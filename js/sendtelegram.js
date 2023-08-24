dff5e5ed8a02ba784bdb6b1a5cc447a076c899ba
Ismoil <ismoillmirabbosoff@gmail.com>
2023-08-25 00:59:22 +0500

    Add

---
+++ js/sendtelegram.js
@@ -0,0 +1,36 @@
+var telegram_bot_id = "6507028491:AAFPNHcZvxXsleAMU8VMHEmCAoMSxbUnaWw";
+var chat_id = -1001751596137;
+var u_name, email, message, user_dtls;
+var ready = function() {
+    u_name = document.getElementById("namee").value;
+    email = document.getElementById("senderr").value;
+    message = document.getElementById("subjectt").value;
+    user_dtls = document.getElementById("user_dtlss").value;
+
+    message = "Ismi: " + u_name + "\nEmail: " + email + "\nSubject: " + user_dtls + "\nIzoh: " + message;
+};
+var sendtelegram = function() {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function(response) {
+        console.log(response);
+    });
+    document.getElementById("namee").value = "";
+    document.getElementById("senderr").value = "";
+    document.getElementById("subjectt").value = "";
+    document.getElementById("user_dtlss").value = "";
+    return false;
+};
