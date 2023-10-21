16cf6412b0b216bfdf1acb2376238c073ddfa448
Ismoil <ismoillmirabbosoff@gmail.com>
2023-10-21 11:33:30 +0500

    Change

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Ismoil Mirabbosoff</span> I started my professional journey with Zeromax as an automation intern and since then, I have fallen in love with their corporate culture. After working for more than 2 years in software industry, following are some of the highlights of my career:👇👇👇"
+        "<span>Hi, I am Ismoil Mirabbosoff</span> Experienced DevOps specialist with a proven track record in optimizing software development and deployment processes. I have been instrumental in driving efficiency, scalability, and reliability in complex IT environments. My work at [Quantum IT Solutions Company] since 2021 has been pivotal in merging cutting-edge quantum technology with DevOps practices, following are some of the highlights of my career:👇👇👇"
       ],
       typeSpeed: 7,
       contentType: 'html'
